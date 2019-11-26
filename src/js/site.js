f839288a0ca128ffccc343e24ae216968e535f5c
Owen Hathaway <owen@ohlawcolorado.com>
2019-11-25 20:07:58 -0700

    added list of blog post excerts from the Wordpress site

--- src/js/site.js
+++ src/js/site.js
@@ -2,9 +2,29 @@ var posts_endpoint = 'https://ohlawcolorado.com/wp-json/wp/v2/posts?_embed&per_p
 
 
 function get_posts() {
+  function linkify(text) {
+    var url_regex = /(\bhttps?:\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    return text.replace(url_regex, function(url){
+      return '<a href="'+url+'">'+url+'</a>';
+    })
+
+  };
+
   $.getJSON(posts_endpoint, function(data) {
     var posts = [];
+
     console.log("WP Posts: ", data);
+    $('#blog-posts .posts').addClass('p-5');
+    $.each(data, function(key, val) {
+      let post_html = "<h2>"+val.title.rendered+"</h2>"+
+                      "<p class='subtitle'>"+val.date+"</p>"+
+                      "<p class='excerpt'>"+linkify(val.excerpt.rendered)+"</p>"+
+                      "<a class='read-post' href='#'>read more</a>"+
+                      "<hr>"
+                      ;
+
+      $('#blog-posts .posts').append(post_html);
+    });
   });
 };
 
@@ -18,7 +38,6 @@ function bind_default_events() {
         toggle.text(function(index, origText){
           return (origText == 'Learn more') ? 'Show less' : 'Learn more';
         });
-        toggle.removeClass('ui-active');
         toggle.fadeIn(1300);
       });
     });
